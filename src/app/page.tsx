ff47f79738d8f931f134306654bba16c167720b0
nain <112144627+Kemzy9@users.noreply.github.com>
2024-11-02 04:01:07 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,7 @@ import React from "react";
 import Hero from "@/app/UI/Hero/page";
 
 
-import Footer from './UI/footer/page';
 
-import Faq from "@/app/UI/faq/page"
-import IntroductionSection from '@/app/UI/IntroductionSection/page'
-
-import Feature from '@/app/UI/feature/page'
-
-import ServicesSection from '@/app/UI/ServicesSection/page'
-
-
-
-import Subscription from '@/app/UI/subscription/page'
-import Review from '@/app/UI/review/page'
-import Sectiondetail from '@/app/UI/sectiondetail/page'
 
 
 
